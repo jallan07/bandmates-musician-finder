96155562d45f7a31d19f01a845f790abb8b5a615
Melanie Hall <hallmt313@gmail.com>
2020-09-26 12:43:40 -0400

    Deezer script tag in index.html

--- main.js
+++ main.js
@@ -12,5 +12,22 @@ $(document).ready(function () {
 
 	// ———————————————————————————— //
 	// ———————————————————————————— //
-	// ———————————————————————————— //
+    // ———————————————————————————— //
+    
+    //API Call to BandsinTown
+    var queryURL = "https://rest.bandsintown.com/artists/" + name + "/?app_id="+ APIkey;
+          var APIkey = "19738606f850fedb0632ecce971be2cb";
+          var name = "Prince";
+      
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+            
+          });
+    
+    //API Call to Deezer
+
+
 });
