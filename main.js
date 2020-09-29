83b788782a403c0a207ea1bed0c0a9146160ac18
Melanie Hall <hallmt313@gmail.com>
2020-09-29 16:21:51 -0400

    Brainstorming back-end design and API usage

--- main.js
+++ main.js
@@ -12,22 +12,43 @@ $(document).ready(function () {
 
 	// ———————————————————————————— //
 	// ———————————————————————————— //
-    // ———————————————————————————— //
+  // ———————————————————————————— //
+
+    //What do we want to accomplish:
+      //STEP ONE: User selects filters and clicks search
+        // Set up click listener for search button to execute functions based on set of parameters
+        //Assign available practice Days to artists called from BandsinTown API using availability()
+        //What criteria are we using to call artists from BandsinTown?
+          //state?
+
+  var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  //Function to randomly assign available days to practice to the artist
+  function availability(){
+    //generates random number
+    var random = Math.floor(Math.random() * 7);
+    //assigns weekday
+    var practiceDay = weekday[random];
+    console.log(practiceDay);
+
+  }
     
-    //API Call to BandsinTown
+    //API Call to BandsinTown - need to adjust to some other call than by name; use state or local?
+        //Then - when search is clicked execute bandsinTown() to pull objects matching state and genre criteria, execute availability to assign a different day of the week to each object in the array and pull in lyrics from another API as the artists favs or some such
+  function bandsinTown(genre){
     var queryURL = "https://rest.bandsintown.com/artists/" + name + "/?app_id="+ APIkey;
-          var APIkey = "19738606f850fedb0632ecce971be2cb";
-          var name = "Prince";
-      
-          $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
-            console.log(response);
-            
-          });
+    var APIkey = "19738606f850fedb0632ecce971be2cb";
+    var name = "Prince";
+        
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+              
+    });
+    availability();
+  }
     
-    //API Call to Deezer
 
 
 });
