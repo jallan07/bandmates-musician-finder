884ff4180a4594eb26ea04906942690117c361d9
Melanie Hall <hallmt313@gmail.com>
2020-09-29 16:27:03 -0400

    Click listener

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  //Global Variable set up:
+  var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 	// ———————————————————————————— //
 	// ———————————————————————————— //
 	// ———————————————————————————— //
@@ -15,23 +17,16 @@ $(document).ready(function () {
   // ———————————————————————————— //
 
     //What do we want to accomplish:
-      //STEP ONE: User selects filters and clicks search
+      //User selects filters and clicks search"
         // Set up click listener for search button to execute functions based on set of parameters
         //Assign available practice Days to artists called from BandsinTown API using availability()
-        //What criteria are we using to call artists from BandsinTown?
-          //state?
+        //What criteria are we using to call artists from BandsinTown? Genre/state?
 
-  var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  //Function to randomly assign available days to practice to the artist
-  function availability(){
-    //generates random number
-    var random = Math.floor(Math.random() * 7);
-    //assigns weekday
-    var practiceDay = weekday[random];
-    console.log(practiceDay);
 
-  }
-    
+  $(".submit-btn").click(function(){
+    bandsinTown();
+
+  })    
     //API Call to BandsinTown - need to adjust to some other call than by name; use state/local and/or genre
         //Then - when search is clicked execute bandsinTown() to pull objects matching state and genre criteria, execute availability to assign a different day of the week to each object in the array and pull in lyrics from another API as the artists favs or some such
   function bandsinTown(genre){
@@ -48,7 +43,13 @@ $(document).ready(function () {
     });
     availability();
   }
-    
-
+  //Function to randomly assign available days to practice to the artist
+  function availability(){
+    //generates random number between 0 and 6
+    var random = Math.floor(Math.random() * 7);
+    //Assigns weekday based on randomly generated index
+    var practiceDay = weekday[random];
+    console.log(practiceDay);
+  }
 
 });
