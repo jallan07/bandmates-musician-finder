bb070faa667b4f3c323706f4d8b934422325066c
Melanie Hall <hallmt313@gmail.com>
2020-09-29 16:40:43 -0400

    More ideas in main.js

--- main.js
+++ main.js
@@ -21,18 +21,19 @@ $(document).ready(function () {
         // Set up click listener for search button to execute functions based on set of parameters
         //Assign available practice Days to artists called from BandsinTown API using availability()
         //What criteria are we using to call artists from BandsinTown? Genre/state?
+        //Since BandsinTown has two endpoints and last.fm requires the user to login - we could have an array of dummy bandmates' names randomly pull from and use in the bandsintown ajax call. It's dummydata on dummydata but not sure how much time we wanna spend on this
 
 
   $(".submit-btn").click(function(){
-    bandsinTown();
+    dummyData();
 
   })    
     //API Call to BandsinTown - need to adjust to some other call than by name; use state/local and/or genre
         //Then - when search is clicked execute bandsinTown() to pull objects matching state and genre criteria, execute availability to assign a different day of the week to each object in the array and pull in lyrics from another API as the artists favs or some such
-  function bandsinTown(genre){
-    var queryURL = "https://rest.bandsintown.com/artists/" + name + "/?app_id="+ APIkey;
-    var APIkey = "19738606f850fedb0632ecce971be2cb";
-    var name = "Prince";
+  function dummyData(genre){
+    var queryURL = "/www.last.fm/api/auth/?api_key="+ APIkey;
+    var APIkey = "a966a6b6197c5e1cee52feef97292710";
+    //var name = "Prince";
         
     $.ajax({
       url: queryURL,
