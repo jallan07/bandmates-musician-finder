79630a4098e4862a55876cb83b8b767ad9a256a5
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-29 21:18:20 -0400

    Updating random profile ajax call. Also created a function to create profile card. Plan to addanother API for random descriptions.

--- main.js
+++ main.js
@@ -30,14 +30,14 @@ $(document).ready(function () {
     profileData();
 
   })    
-    //API Call to BandsinTown - need to adjust to some other call than by name; use state/local and/or genre
+    //API Call to Random Profile - need to adjust to some other call than by name; use state/local and/or genre
         //Then - when search is clicked execute bandsinTown() to pull objects matching state and genre criteria, execute availability to assign a different day of the week to each object in the array and pull in lyrics from another API as the artists favs or some such
   function profileData(){
-    var howMany = 100;
+    var howMany = 10;
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": "https://dawn2k-random-german-profiles-and-names-generator-v1.p.rapidapi.com/?count="+ howMany +"&gender=b&maxage=40&minage=30&cc=all&email=gmail.com%252Cyahoo.com&pwlen=12&ip=a&phone=l%252Ct%252Co&uuid=false&lic=false&color=false&seed=helloworld&images=false&format=json",
+      "url": `https://dawn2k-random-german-profiles-and-names-generator-v1.p.rapidapi.com/?count=${howMany}&gender=b&maxage=40&minage=30&cc=all&email=gmail.com&pwlen=12&ip=a&phone=l%252Ct%252Co&uuid=false&lic=false&color=false&seed=helloworld&images=false&format=json`,
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "dawn2k-random-german-profiles-and-names-generator-v1.p.rapidapi.com",
@@ -45,9 +45,10 @@ $(document).ready(function () {
       }
     }
     
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
       console.log(response);
-    });
+      createCard(response);
+    })
     dayandGenre();
   }
 
@@ -74,4 +75,42 @@ $(document).ready(function () {
     var thisGenre = genre[random2];
   }
 
+  function createCard(apiData) {
+    for (var i = 0; i < apiData.length; i++) {
+
+      var description =  `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+          specimen book.`;
+      var userCard = 
+      `<div class="columns is-vcentered">` +
+      `<div class="column is-4">` +
+        `<div class="card">` +
+            `<div class="card-image">` +
+              `<figure class="image is-4by3">` +
+                `<img src="img/card-main.jpg" alt="Placeholder image"/>` +
+              `</figure>` +
+            `</div>` +
+            `<div class="card-content">` +
+              `<div class="media">` +
+                `<div class="media-left">` +
+                  `<figure class="image is-48x48">` +
+                    `<img src="img/card-main.jpg" alt="Placeholder image" />` +
+                  `</figure>` +
+                `</div>` +
+                `<div class="media-content">` +
+                  `<p class="title is-4">${apiData[i].firstname} ${apiData[i].lastname}</p>` +
+                  `<p class="subtitle is-6">@${apiData[i].username}</p>` +
+                `</div>` +
+              `</div>` +
+
+              `<div class="content">` +
+                `${description}` +
+                `<br/>` +
+              `</div>` +
+        `</div>` +
+    ` </div>` +
+    ` </div>` ;
+
+    // $('body').append(userCard);
+  }
+  };
 });
