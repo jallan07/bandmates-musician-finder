d272d94039ac19c9d8f96fc5c72d713fe1bf8767
Patrick W <pat.realrealestatellc@gmail.com>
2020-10-03 12:00:33 -0400

    Adding js files. Functions added: emailBandmate, storeUserInfo, removeCard

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 $(document).ready(function () {
+
+  if (!localStorage.getItem('genreInput') || !localStorage.getItem('dayInput')) {
+    return;
+  } else {
+    $('#genre-select').val(localStorage.getItem('genreInput'));
+    $('#day-select').val(localStorage.getItem('dayInput'));
+  };    
+
   //Global Variable set up:
   // var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   // var genre = ["Alternative", "Blues", "Country", "Electronic", "Hip Hop", "Indie", "Pop", "Punk","Rock"];
-	// ———————————————————————————— //
-	// ———————————————————————————— //
-	// ———————————————————————————— //
-
-	// Check for click events on the navbar burger icon
-	$(".navbar-burger").click(function () {
-		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-		$(".navbar-burger").toggleClass("is-active");
-		$(".navbar-menu").toggleClass("is-active");
-	});
-
-	// ———————————————————————————— //
-	// ———————————————————————————— //
+  // ———————————————————————————— //
+  // ———————————————————————————— //
   // ———————————————————————————— //
 
-    //What do we want to accomplish:
-      //User selects filters and clicks search"
-        //Set up click listener for search button to execute functions based on set of parameters
-        //Assign available practice Days to "artists"/profiles
-          //RandomAPI or German Profile generator for main data in cards
-      //Second API: Google Maps API
-        //Plot where band mates are, or locations where you could meet to jam or record 
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
+  // ———————————————————————————— //
+  // ———————————————————————————— //
+  // ———————————————————————————— //
+
+  //What do we want to accomplish:
+    //User selects filters and clicks search"
+      //Set up click listener for search button to execute functions based on set of parameters
+      //Assign available practice Days to "artists"/profiles
+        //RandomAPI or German Profile generator for main data in cards
+    //Second API: Google Maps API
+      //Plot where band mates are, or locations where you could meet to jam or record 
 
 
-  $(".submit-btn").click(function(event){
+  $(".submit-btn").click(function(){
 
     if($('#genre-select').val() !== "Placeholder" && $('#day-select').val() !== "Placeholder") {
 
@@ -34,15 +42,16 @@ $(document).ready(function () {
         $('.results-container').fadeIn('transition'); 
       });
       profileData();
-      // createCard();  
+      storeUserAvailablity();
     } else {
       return;
     };
   });   
+
     //API Call to Random Profile - need to adjust to some other call than by name; use state/local and/or genre
         //Then - when search is clicked execute bandsinTown() to pull objects matching state and genre criteria, execute availability to assign a different day of the week to each object in the array and pull in lyrics from another API as the artists favs or some such
   function profileData(){
-    var howMany = 1;
+    var howMany = 3;
     var settings = {
       "async": true,
       "crossDomain": true,
@@ -55,26 +64,21 @@ $(document).ready(function () {
     }
     
     $.ajax(settings).done(function(response) {
+      console.log(response);
       createCard(response);
     })
   };
 
-  //In future state this would be based on geolocation of the user- for testing purposes the location is a set point
-  var location1 = "Pariser Platz, 10117 Berlin, Germany";
-  //The end location or destination is based in using the profileData function
-  function locationData(response){
-    var start = location1;
-    var end = response.location .street.number + " " + response.location.street.name + ", " + response.location.city + ", Germany";
+  function locationData(){
+    var start = "";
+    var end = "";
     var APIkey = "AIzaSyBvxteS-wirlxIYnsck8jJXEn7JB3JLdR0";
-    //remove https://cors-anywhere.herokuapp.com/ when we put it in the master
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + start + "&destinations=" + end + "&key=" + APIkey;
+    var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + start + "&destinations=" + end + "&key=" + APIkey;
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(result) {
-      var distance = result.rows[0].elements[0].distance.text;
-      console.log(distance);
-      return distance;
+    }).then(function(response) {
+      console.log(response);
     })
   }
 
@@ -83,43 +87,48 @@ $(document).ready(function () {
         var descriptionText = randomText();
         // var randomDay = weekday[Math.floor(Math.random() * 7)];
         // var randomGenre = genre[Math.floor(Math.random() * 9)];
-        var locationText = locationData(apiData[i]); //undefined because it is called before the value is available, ie the ajax call in locationData takes longer than the others
-        var userCard = 	`<div class="columns is-vcentered">` +
-          `<div class="column is-4">` +
-            `<div class="card">` +
-							`<div class="card-image">` +
-								`<figure class="image is-4by3">` +
-									`<img src="${apiData[i].image}" alt="Placeholder image" />` +
-								`</figure>` +
-							`</div>` +
-							`<div class="card-content has-text-centered">` +
-								`<div class="media">` +
-									`<div class="media-content">` +
-										`<p class="title is-3">${apiData[i].firstname} ${apiData[i].lastname}</p>` +
-                    `<p class="subtitle is-5">@${apiData[i].username}</p>` +
-                    `<p>${$('#genre-select').val()}</p>` +
-									`</div>` +
-									`<div class="media-right has-text-centered">` +
-										`<p class="title is-3">` +
-											`<i class="fas fa-directions"></i>` +
-										`</p>` +
-										`<p class="subtitle is-5" id="distance">${locationText}</p>` +
-									`</div>` +
-								`</div>` +
-                `<div id="descriptionDiv">` +
-									`<p class="card-bio is-6 description">`+
-										`${descriptionText}` +
-										`<a>#pop</a>` +
-										`<a>#r&b</a>` +
-									`</p>` + 
-								`</div>` +
-							`</div>` +
+
+        var userCard = 	`<div class="column is-one-third">` +
+        `<div class="card">` +
+          `<div class="card-image">` +
+            `<figure class="image is-4by3">` +
+              `<img src="${apiData[i].image}" alt="Placeholder image" />` +
+            `</figure>` +
+          `</div>` +
+          `<div class="card-content has-text-centered">` +
+            `<div class="media">` +
+              `<div class="media-content">` +
+                `<p class="title is-5 name" value="${apiData[i].firstname}">${apiData[i].firstname} ${apiData[i].lastname}</p>` +
+                `<p class="subtitle is-6">@${apiData[i].username}</p>` +
+              `</div>` +
+              `<div class="media-right has-text-centered">` +
+                `<p class="title is-5">` +
+                  `<i class="fas fa-directions"></i>` +
+                `</p>` +
+                `<p class="subtitle is-6" id="distance">4.4 miles</p>` +
+              `</div>` +
+            `</div>` +
+            `<div>` +
+              `<p class="subtitle card-bio is-6">` +
+                `<b>Available on:</b> ${$('#day-select').val()}'s` +
+                `<p class="description">${descriptionText}</p>` +
+                `<a>#${$('#genre-select').val()}</a>` +
+                // <!-- dynamically insert the genre into the a tag above (add a hashtag to the beginning of it to make it look searchable) -->
+              `</p>` +
+            `</div>` +
+            `<div class="card-container">` +
+              `<button class="removeBtn"><i class="fas fa-times fa-2x"></i></button>` +
+              `<button class="emailBtn" value="${apiData[i].email}"><i class="fas fa-envelope-square fa-2x"></i></button>` +
             `</div>` +
           `</div>` +
-          `</div>`;
+        `</div>` +
+      `</div>`;
 
-       $('#cardContainer').append(userCard);
+      $('#cardContainer').append(userCard);
     };
+
+    $('.emailBtn').click(emailBandMate);
+    $('.removeBtn').click(removeCard);
   };
 
   function randomText() {
@@ -137,5 +146,27 @@ $(document).ready(function () {
 
         $(`.description`).text(trimmedString);
     });
+  };
+
+  function emailBandMate() {
+    console.log();
+    console.log($(this).val())
+    var email = $(this).val();
+    var subject = "Let's get together!";
+    var emailBody = "Hey!" + " I see we have the same availability. What time works for you on " + $('#day-select').val() + 
+        " so we can get together and make the some music! Rock On!";
+
+    window.location = `mailto:${email}?subject=${subject}&body=${emailBody}`;
+    $(this).parent().parent().prev().parent();
+  };
+
+  function removeCard() {
+    console.log($(this))
+    $(this).parent().parent().prev().parent().fadeOut();
+  };
+
+  function storeUserAvailablity() {
+    localStorage.setItem('genreInput' , $('#genre-select').val());
+    localStorage.setItem('dayInput' , $('#day-select').val());
   };
 });
